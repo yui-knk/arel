082a81aa2db7859697115c3d1a23ff91fcfa5c6a
Aaron Patterson <aaron.patterson@gmail.com>
2010-09-22 14:57:15 -0700

    hashes need to be bound as hashes. :'(

--- lib/arel/algebra/core_extensions/hash.rb
+++ lib/arel/algebra/core_extensions/hash.rb
@@ -1,7 +1,11 @@
 class Hash
   def bind(relation)
     Hash[map { |key, value|
-      [key.bind(relation), value.bind(relation)]
+      value = Hash === value ?
+        Arel::Value.new(value, relation) :
+        value.bind(relation)
+
+      [key.bind(relation), value]
     }]
   end
 end
